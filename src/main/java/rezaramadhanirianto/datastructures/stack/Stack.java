9b4d019e3d99f409725412e05341f54061e53b4b
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2022-06-05 20:23:41 +0700

    add ArrayStack and test case

---
+++ src/main/java/rezaramadhanirianto/datastructures/stack/Stack.java
@@ -0,0 +1,9 @@
+package rezaramadhanirianto.datastructures.stack;
+
+public interface Stack<T> {
+    public int size();
+    public boolean isEmpty();
+    public void push(T elem);
+    public T pop();
+    public T peek();
+}
