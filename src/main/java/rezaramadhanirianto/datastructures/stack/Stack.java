1fe0d7629faa4352751d342935daac7cdcfbcf4b
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2022-06-08 06:31:14 +0700

    deleting old files

--- src/main/java/rezaramadhanirianto/datastructures/stack/Stack.java
+++
@@ -1,9 +0,0 @@
-package rezaramadhanirianto.datastructures.stack;
-
-public interface Stack<T> {
-    public int size();
-    public boolean isEmpty();
-    public void push(T elem);
-    public T pop();
-    public T peek();
-}
