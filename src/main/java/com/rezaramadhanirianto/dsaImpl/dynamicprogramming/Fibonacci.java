594c3287e57fc41a96578fc0e1a8e25fa03cdb3b
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2022-07-11 01:35:53 +0700

    add fibonacci dynamic programming

---
+++ src/main/java/com/rezaramadhanirianto/dsaImpl/dynamicprogramming/Fibonacci.java
@@ -0,0 +1,22 @@
+package com.rezaramadhanirianto.dsaImpl.dynamicprogramming;
+
+public class Fibonacci {
+    final int max = 1000;
+    int[] fib = new int[max];
+
+    int fibonacci(int num){
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+
+        if(fib[num] != 0) return fib[num];
+
+        fib[num] = fibonacci(num - 1) + fibonacci(num - 2);
+        return fib[num];
+    }
+
+    public static void main(String[] args){
+        Fibonacci fibonacci = new Fibonacci();
+        var output = fibonacci.fibonacci(5);
+        System.out.println(output);
+    }
+}
