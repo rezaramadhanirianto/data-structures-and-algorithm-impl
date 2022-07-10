5243fcd25c4a62948d926eecd68b90a5a1d96b3c
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2022-07-11 01:35:43 +0700

    fix fibonacci recursion

--- src/main/java/com/rezaramadhanirianto/dsaImpl/algorithm/recursion/Fibonacci.java
+++ src/main/java/com/rezaramadhanirianto/dsaImpl/algorithm/recursion/Fibonacci.java
@@ -1,11 +1,9 @@
 package com.rezaramadhanirianto.dsaImpl.algorithm.recursion;
 
-import java.util.Arrays;
-
 public class Fibonacci {
 
     int fibonacci(int x){
-        if(x <= 1)
+        if(x < 2)
             return x;
         return fibonacci(x - 1) + fibonacci(x -2);
     }
